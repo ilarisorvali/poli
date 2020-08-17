3621cfdb544b185670edfbc18825e9958e2f4e0d
sorvali1 <ilari.sorvali@aalto.fi>
2020-08-17 15:46:52 +0300

    >implying i use commit messages

---
+++ precache-manifest.910943585c1eefa8219c1ed9a8651e5f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17b4b365c51a32728ed76f0059575db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "557287d8f33d60e5f239",
+    "url": "/static/css/main.4e3af4c2.chunk.css"
+  },
+  {
+    "revision": "40430faa5789d441ed50",
+    "url": "/static/js/2.58d0f5b8.chunk.js"
+  },
+  {
+    "revision": "557287d8f33d60e5f239",
+    "url": "/static/js/main.df9be2cf.chunk.js"
+  },
+  {
+    "revision": "bd890b964538b265de41",
+    "url": "/static/js/runtime~main.606749f5.js"
+  },
+  {
+    "revision": "b4f9eb8ce027016ab9b9860817451d07",
+    "url": "/static/media/Proxima_Nova_Extrabold.b4f9eb8c.otf"
+  },
+  {
+    "revision": "6a386899746222073dd64c5f74d1a69d",
+    "url": "/static/media/Proxima_Nova_Semibold.6a386899.otf"
+  },
+  {
+    "revision": "8f0bc01ce5e5becef482d277cb72b728",
+    "url": "/static/media/Proxima_Nova_Thin.8f0bc01c.otf"
+  },
+  {
+    "revision": "c561ab59aee48b110b51c783a518b513",
+    "url": "/static/media/logoo.c561ab59.svg"
+  },
+  {
+    "revision": "1a3a063e57d7ad41306a263013bf7825",
+    "url": "/static/media/nettisivutausta.1a3a063e.png"
+  },
+  {
+    "revision": "f5b6e33818e24073ca010f86c27348ad",
+    "url": "/static/media/puhelintausta.f5b6e338.png"
+  }
+]);
